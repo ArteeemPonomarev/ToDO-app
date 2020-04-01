aef58b99a705ee4b74bad22470a89677440bd30a
ArteeemPonomarev <artponomarev1994@gmail.com>
2020-04-02 02:53:58 +0400

    Updates

---
+++ precache-manifest.8a76583c6b9935aa7a2ee7eed194313d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38a6aba2ac006f1ddb4649c61f68164c",
+    "url": "/ToDO-app/index.html"
+  },
+  {
+    "revision": "9a8f22ef676518e75f3e",
+    "url": "/ToDO-app/static/css/main.02d8ba0e.chunk.css"
+  },
+  {
+    "revision": "52343064667404f2fb24",
+    "url": "/ToDO-app/static/js/2.10604bf3.chunk.js"
+  },
+  {
+    "revision": "9a8f22ef676518e75f3e",
+    "url": "/ToDO-app/static/js/main.bddf55d8.chunk.js"
+  },
+  {
+    "revision": "99a270f9c16299f989a5",
+    "url": "/ToDO-app/static/js/runtime~main.763d894b.js"
+  }
+]);
